b065261d40194f394c4f5121fe59a73b6b036ad5
Roger Braun <roger.braun@student.uni-tuebingen.de>
2010-07-18 19:12:45 +0200

    Modified for more general usage

--- mandelbrot.rb
+++ mandelbrot.rb
@@ -1,51 +1,41 @@
+#encoding: utf-8
 #Idea and modified from http://blog.mostof.it/mandelbrot-set-in-ruby-and-haskell
 require 'rubygems'
 require 'complex'
 require 'RMagick'
 include Magick
  
-# this is the interesting area of the complex plane
-X_START = -2.0
-X_END = 0.5
-Y_START = -1.0
-Y_END = 1.0
- 
-# wanted image dimensions
-WIDTH = 800.0
-HEIGHT = 600.0
- 
-# set this to higher values and sleep well :)
-ITERATIONS = 200 
-COLORS = 0xfffff / ITERATIONS
- 
-STEP_X = (X_END - X_START) / WIDTH
-STEP_Y = (Y_END - Y_START) / HEIGHT
- 
-def mandelbrot(surface,width,height,x1,x2,y1,y2)
+def mandelbrot(surface,width = 800,height = 600, x1 = -2.0,x2 = 0.5,y1 = -1.0 ,y2 = 1.0, iterations = 200)
 
   xsize = (x2 - x1) / width.to_f
   ysize = (y2 - y1) / height.to_f
+  colors = 0xffffff / iterations
 
   height.to_i.times do |y|
     puts "Line #{y}"
     width.to_i.times do |x|
-      escape = escapes?(Complex((x * xsize) + x1, (y * ysize) + y1))
-      Draw.new.fill("#%06x" % (escape * COLORS)).point(x,y).draw(surface) if escape 
+      escape = escapes?(Complex((x * xsize) + x1, (y * ysize) + y1), iterations)
+      Draw.new.fill("#%06x" % (escape * colors)).point(x,y).draw(surface) if escape 
     end
   end
 end
 
-def escapes?(c)
+def escapes?(c, iterations = 200)
+
+  # quick check to see if the point is in the main bulb
+  #q = ((c.real - 1/4) ** 2) + (c.imag ** 2)
+  #return false if q * (q + (c.real - 1/4)) < (1/4 * c.imag ** 2)
+  # it isn't, so compute
   z = Complex(0)
-  ITERATIONS.times do |x|
+  iterations.times do |x|
     return x if z.abs > 2
     z = z*z + c
   end
-  false
+  0
 end
  
-complex_plane = Image.new(WIDTH.to_i, HEIGHT.to_i)
+complex_plane = Image.new(8000,6000)
 
-mandelbrot(complex_plane,WIDTH,HEIGHT,X_START,X_END,Y_START,Y_END)
+mandelbrot(complex_plane, 8000, 6000)
  
 complex_plane.write("mandelbrot.png")
