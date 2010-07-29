af27ee6061d52228ad9a685873e290ec61205660
Roger Braun <roger.braun@student.uni-tuebingen.de>
2010-07-29 17:19:30 +0200

    Removed requirement for 'complex' gem

--- mandelbrot.rb
+++ mandelbrot.rb
@@ -1,7 +1,6 @@
 #encoding: utf-8
 #Idea and modified from http://blog.mostof.it/mandelbrot-set-in-ruby-and-haskell
 require 'rubygems'
-require 'complex'
 require 'RMagick'
 include Magick
  
@@ -14,28 +13,31 @@ def mandelbrot(surface,width = 800,height = 600, x1 = -2.0,x2 = 0.5,y1 = -1.0 ,y
   height.to_i.times do |y|
     puts "Line #{y}"
     width.to_i.times do |x|
-      escape = escapes?(Complex((x * xsize) + x1, (y * ysize) + y1), iterations)
+      escape = escapes?((x * xsize) + x1, (y * ysize) + y1, iterations)
       Draw.new.fill("#%06x" % (escape * colors)).point(x,y).draw(surface) if escape 
     end
   end
 end
 
-def escapes?(c, iterations = 200)
+def escapes?(x0,y0, iterations = 200)
 
   # quick check to see if the point is in the main bulb
   #q = ((c.real - 1/4) ** 2) + (c.imag ** 2)
   #return false if q * (q + (c.real - 1/4)) < (1/4 * c.imag ** 2)
   # it isn't, so compute
-  z = Complex(0)
-  iterations.times do |x|
-    return x if z.abs > 2
-    z = z*z + c
+  x = 0
+  y = 0
+  iterations.times do |count|
+    return count if x * x + y * y > 4
+    xtemp = x * x - y * y + x0
+    y = 2 * x * y + y0
+    x = xtemp
   end
   0
 end
  
-complex_plane = Image.new(8000,6000)
+complex_plane = Image.new(800,600)
 
-mandelbrot(complex_plane, 8000, 6000)
+mandelbrot(complex_plane, 800, 602)
  
 complex_plane.write("mandelbrot.png")
